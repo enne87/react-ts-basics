959693591ae4209051af7775afad4ab7014e5ca0
Florian Ennemoser <florian.ennemoser@xemedo.com>
2023-11-07 18:29:07 +0100

    Updated code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
@@ -25,7 +26,20 @@ const concepts = [
 
 function App() {
     return (
-        <></>
+        <div>
+            <header>
+                <img src={keyConceptsImage} alt="Medal badge with a star" />
+                <h1>Key React Concepts</h1>
+                <p>Selected key React concepts you should know about</p>
+            </header>
+            <ul id="concepts">
+                <li className="concept">
+                    <img src="TODO: IMAGE" alt="TODO: TITLE" />
+                    <h2>TODO: TITLE</h2>
+                    <p>TODO: DESCRIPTION</p>
+                </li>
+            </ul>
+        </div>
     );
 }
 
